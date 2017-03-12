b5c59e136db61089388cc807a6949098404252e5
DanailPenev <fcbdelrio@gmail.com>
2017-03-12 07:44:21 +0000

    Prompt for username and password

--- FrontEndIonic/src/pages/taxi-info/taxi-info.ts
+++ FrontEndIonic/src/pages/taxi-info/taxi-info.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { ServerRequest } from '../service/server-request';
 import { HomePage } from '../taxi-request/taxi-request';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-taxi-info',
@@ -13,13 +14,47 @@ export class TaxiInfoPage {
     fromValue: string;
     toValue: string;
 
-    constructor(public navCtrl: NavController, private navParams: NavParams) {
+    constructor(public navCtrl: NavController, private navParams: NavParams, public alertCtrl: AlertController) {
       console.log(navParams);
       console.log(navParams.get('data'));
       this.companies = navParams.get('data');
       console.log(this.fromValue);
       console.log(this.toValue);
       console.log(this.companies);
+    }
+
+    showPrompt(){
+      let prompt = this.alertCtrl.create({
+        title: 'Login',
+        message: 'Enter your username and password',
+        inputs: [
+        {
+          name: 'username',
+          placeholder: 'Username',
+        },
+        {
+          name: 'password',
+          type: 'password',
+          placeholder: 'password',
+        },
+        ],
+        buttons: [
+          {
+            text: 'Cancel',
+            handler: data => {
+              console.log("CANCEL");
+            }
+          },
+          {
+            text: 'Save',
+            handler: data => {
+              console.log("SAVE");
+              // change page
+            }
+          }
+        ]
+      });
+      prompt.present();
     }
 
 }
